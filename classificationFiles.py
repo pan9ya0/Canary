042f61650350e2bc0483c5a33bf0b776be93d336
pan9ya0 <3543360132@qq.com>
2020-02-08 19:00:50 +0800

    file classification module

---
+++ classificationFiles.py
@@ -0,0 +1,13 @@
+# encoding=utf-8
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/2/8 13:36
+# @Author  : Pan9ya0
+# @Email   : su33e7@gmail.com
+# @File    : classificationFiles.py
+
+
+class classificationFiles:
+
+    def __init__(self,path_to_file):
+        self. path = path_to_file
